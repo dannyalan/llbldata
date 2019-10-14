abab04cfa68ce48090d415c0d48d951482b28e8e
Danny Alan <danny.alan@inosculateltd.co.uk>
2019-10-14 23:16:09 +0100

    Updated permissions

--- copyData.sh
+++ copyData.sh
@@ -1,2 +1,2 @@
-cp -r articleData /src/build/volume/articleData/
-cp -r imageUploads /src/build/volume/imageUploads/
+cp -r articleData/* /src/build/volume/articleData/
+cp -r imageUploads/* /src/build/volume/imageUploads/
